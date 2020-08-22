6374bf2cf87fa076abd2c40da8e27a76145127f0
emandere <emandere@gmail.com>
2020-08-22 05:02:15 -0400

    Added fancy material stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatButtonModule} from "@angular/material/button";
+import { MatListModule} from "@angular/material/list";
+import { MatSelectModule} from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { MatDialogModule}  from '@angular/material/dialog';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +23,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatListModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
